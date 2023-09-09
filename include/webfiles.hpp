50c6d3ee21f8aec9db39327483dc4a142c43d1cb
Panaintescu Adrian Valentin <adrian.cert@gmail.com>
2023-09-09 10:07:46 +0300

    WRC-009 organize web files

---
+++ include/webfiles.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <Arduino.h>
+
+const char index_html[] PROGMEM = R"rawliteral(
+)rawliteral";
+
+const char index_css[] PROGMEM = R"rawliteral(
+)rawliteral";
+
+const char index_js[] PROGMEM = R"rawliteral(
+)rawliteral";
---
+++ readme.md
@@ -0,0 +1,8 @@
+
+
+## Compile
+
+Before complile and uploading run the command
+```bash
+python .\scripts\data_sync.py sync include/webfiles.hpp -i data/index.html:index_html -i data/index.js:index_js -i data/style.css:index_css
+```
